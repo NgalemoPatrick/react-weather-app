8e86e5b7068a1bff3ab84b4eaaf49207030b0bf5
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-07-25 23:34:00 -0500

    first commit delete all the unececary files and set the api url variable

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {useState, useEffet} from 'react'
+
+// Variable to store the Api url
+const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`
+
+function App() {
+  return (
+    <div className="App">
+     Hello
+    </div>
+  );
+}
+
+export default App;
